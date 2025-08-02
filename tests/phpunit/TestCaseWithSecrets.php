fa194c9e47e95875974f3be588fd027176da7024
Phillmin <privat@phillip-thiele.de>
2025-08-03 01:54:45 +0200

    [test]: clean up TestCaseWithSecrets by removing unused methods and imports
    - Removed createToken() and decryptToken() helper methods
    - Cleaned up unused imports (JwtTokenFactory, JwtTokenParser, EncryptedJwtBundle)
    - Simplifies test base class to only retain relevant setup logic

--- tests/phpunit/TestCaseWithSecrets.php
+++ tests/phpunit/TestCaseWithSecrets.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace Tests\phpunit;
 
 use PHPUnit\Framework\TestCase;
-use Phithi92\JsonWebToken\JwtTokenFactory;
-use Phithi92\JsonWebToken\EncryptedJwtBundle;
 use Phithi92\JsonWebToken\JwtAlgorithmManager;
 use Phithi92\JsonWebToken\JwtPayload;
-use Phithi92\JsonWebToken\JwtTokenParser;
 use Tests\Helpers\KeyProvider;
 
 class TestCaseWithSecrets extends TestCase
@@ -40,31 +37,6 @@ class TestCaseWithSecrets extends TestCase
         }
 
         $this->manager = $manager;
-    }
-
-
-    public function createToken(
-        string $algorithm
-    ): string {
-        $payload = $this->getPayload();
-
-        $bundle = JwtTokenFactory::createToken($this->manager, $payload, $algorithm);
-
-        $decryptedToken = JwtTokenParser::serialize($bundle);
-
-        $this->assertIsString($decryptedToken, 'Token sollte ein String sein');
-        return $decryptedToken;
-    }
-
-    public function decryptToken(
-        string $token,
-        ?JwtAlgorithmManager $manager = null
-    ) {
-        $resolvedManager = $manager ?? $this->manager;
-
-        $decryptedToken = JwtTokenFactory::decryptToken($resolvedManager, $token);
-
-        $this->assertInstanceOf(EncryptedJwtBundle::class, $decryptedToken);
     }
 
     protected function getPayload(): JwtPayload
