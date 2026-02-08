224fadef1f117d6ba2000431347b14dd4af57161
Phillmin <privat@phillip-thiele.de>
2026-02-08 07:05:01 +0100

    feat(token): add JwtTokenKind::fromTypeOrFail()
    - validate type input and throw InvalidArgumentException

--- src/Token/JwtTokenKind.php
+++ src/Token/JwtTokenKind.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace Phithi92\JsonWebToken\Token;
 
+use InvalidArgumentException;
+use ValueError;
+
+use function get_debug_type;
+use function is_int;
+use function is_string;
+
 enum JwtTokenKind: string
 {
     case JWS = 'JWS';
     case JWE = 'JWE';
-    case JWT = 'JWT'; // oft synonym zu JWS
+    case JWT = 'JWT';
 
     public function partCount(): int
     {
@@ -16,6 +23,33 @@ enum JwtTokenKind: string
             self::JWS, self::JWT => 3,
             self::JWE => 5,
         };
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public static function fromTypeOrFail(mixed $type): self
+    {
+        if (! is_string($type) && ! is_int($type)) {
+            throw new InvalidArgumentException(
+                sprintf('Unsupported JWT type (expected string|int), got: %s', get_debug_type($type))
+            );
+        }
+
+        $type = (string) $type;
+
+        if ($type === '') {
+            throw new InvalidArgumentException('Unsupported JWT type: empty string');
+        }
+
+        try {
+            return self::from($type);
+        } catch (ValueError $e) {
+            throw new InvalidArgumentException(
+                sprintf('Unsupported JWT type: %s', $type),
+                previous: $e
+            );
+        }
     }
 
     public function isSignatureToken(): bool
