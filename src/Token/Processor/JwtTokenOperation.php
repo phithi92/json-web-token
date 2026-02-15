adba335e77dbf19e8c9c073724013019662bca7a
Phillmin <privat@phillip-thiele.de>
2026-02-15 09:34:24 +0100

    feat(token): formalize JwtTokenOperation processing hooks
    - Add `resolveAlgorithm(JwtBundle): string` to expose algorithm resolution as part of the operation contract
    - Add `dispatchHandlers(string, JwtBundle): JwtBundle` to standardize handler pipeline dispatch across processors
    - Import `JwtBundle` to enable strict typing for operation implementations

--- src/Token/Processor/JwtTokenOperation.php
+++ src/Token/Processor/JwtTokenOperation.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Phithi92\JsonWebToken\Token\Processor;
 
+use Phithi92\JsonWebToken\Token\JwtBundle;
+
 interface JwtTokenOperation
 {
+    public function resolveAlgorithm(JwtBundle $bundle): string;
+    public function dispatchHandlers(string $algorithm, JwtBundle $jwtBundle): JwtBundle;
 }
